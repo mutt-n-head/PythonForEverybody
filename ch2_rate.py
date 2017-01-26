b01fd213a6362d8a176a7e51829540a768c3e92a
Lewis Isner <lewis.isner@gmail.com>
2017-01-26 17:25:33 -0500

    Initial Commit

---
+++ ch2_rate.py
@@ -0,0 +1,9 @@
+# Assignment for week 4
+hrs = raw_input('Enter hours:')
+rate = raw_input('Enter rate:')
+
+# All raw input taken in as String type, needs conversion.
+hrs = int(hrs)
+rate = float(rate)
+
+print hrs * rate
---
+++ sample.py
@@ -0,0 +1,8 @@
+my_int = 10
+print("The value of the variable my_int is")
+print(my_int)
+my_float = 1.22
+print("The value of my float is:  %1.1f and the other is %1.1f" % (my_float , my_int))
+
+
+
