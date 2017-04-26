16d1ddc5ecda22bc683ae5be06d26248ea888da8
Lewis Isner <lewis.isner@gmail.com>
2017-04-26 14:43:23 -0600

    Modified style on 11_1 and added Soup and 12_1 for next part of assignment.

--- webData/problem_11_1.py
+++ webData/problem_11_1.py
@@ -1,4 +1,5 @@
 import re
+import math
 
 def getTotal(filename):
     fh = open(filename, "r")
@@ -18,7 +19,30 @@ print getTotal("regex_sum_353704.txt")
 # print "The total for the assignment file is:", getTotal('regex_sum_353704.txt')
 
 
+# Amount of time elapsed on Earth
+# T = T0 / sqrt(1 - (v**2/c**2))
+# Reverse it.
+T0 = 30
+multiplier = math.sqrt(1 - (math.pow(150000, 2)/math.pow(299000, 2)))
+print "The multiplier is: ", multiplier
+T = (T0/multiplier)
+print "The time they travel is: ", T
 
+# Playing with lists
+mixList = []
+mixList.append(2)
+mixList.append('two')
+print mixList
 
+# Messing with dictionaries
+mixDict = {}
+mixDict['firstKey'] = 1
+mixDict['aKey'] = 2
+for item in sorted(mixDict):
+    print item, "", mixDict[item]
 
+myTuple = (2, 3, 4)
+myTupleList = []
+myTupleList.append(myTuple)
+print myTupleList
 
