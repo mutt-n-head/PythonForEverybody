de66a56508a2583f41a1474cf7a5c2762650f335
Lewis Isner <lewis.isner@gmail.com>
2017-05-31 08:06:35 -0400

    Updated to reduce code.

--- databases/emaildb.py
+++ databases/emaildb.py
@@ -2,26 +2,24 @@ import sqlite3
 
 conn = sqlite3.connect('/Users/lisner/Desktop/py4all/databases/emaildb.sqlite')
 cur = conn.cursor()
-
-cur.execute('''
-DROP TABLE IF EXISTS Counts''')
-
-cur.execute('''
-CREATE TABLE Counts (org TEXT, count INTEGER)''')
-
+cur.execute('''DROP TABLE IF EXISTS Counts''')
+cur.execute('''CREATE TABLE Counts (org TEXT, count INTEGER)''')
 fname = raw_input('Enter file name: ')
-if ( len(fname) < 1 ) : fname = 'mbox.txt'
+
+if (len(fname) < 1 ) :
+    fname = 'mbox.txt'
+
 fh = open(fname)
+
 for line in fh:
     if not line.startswith('From: ') : continue
     pieces = line.split()
     email = pieces[1]
     print email
-    orgs = email.split('@')
-    org = orgs[1]
-    # print 'org is', org
+    org = email.split('@')[1]
     cur.execute('SELECT count FROM Counts WHERE org = ? ', (org, ))
     row = cur.fetchone()
+
     if row is None:
         cur.execute('''INSERT INTO Counts (org, count) VALUES ( ?, 1 )''', (org, ))
     else :
@@ -37,6 +35,7 @@ sqlstr = 'SELECT org, count FROM Counts ORDER BY count DESC LIMIT 10'
 
 print
 print "Counts:"
+
 for row in cur.execute(sqlstr) :
     print str(row[0]), row[1]
 
