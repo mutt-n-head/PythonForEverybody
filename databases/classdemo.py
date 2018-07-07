0215cb929dbacb320c03da9a73953a39ea07cdc0
mutt-n-head <lewis.isner@gmail.com>
2018-07-07 11:28:13 -0400

    Added formatting nifty stuff

--- databases/classdemo.py
+++ databases/classdemo.py
@@ -28,3 +28,14 @@ aa.add_one()
 aa.create_string('... and yet more')
 print(aa.someStr)
 print(aa.x)
+
+# Neat trick for printing fancy wise
+# Note the spread operation on the data dictionary object
+
+data = {
+    'fname': 'Joe',
+    'lname': 'Sixpack',
+    'age': 23
+}
+
+print('Resuts are {fname} and {lname} who is {age} years old'.format(**data))
